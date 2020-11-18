178f1f610a1f9dfc012067a9d5263974d7c7855a
Vivan <vivanvatsa@gmail.com>
2020-11-18 14:48:22 +0530

    CMAKE

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Vivan/Documents/GitHub/teams-automated-attender
+# Build directory: C:/Users/Vivan/Documents/GitHub/teams-automated-attender/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
